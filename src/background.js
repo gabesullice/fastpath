b5d32030235e6811f7607d7f58f74953db5f150b
Gabriel Sullice <gabriel@sullice.com>
2026-02-10 11:21:41 +0100

    extract shared copy/paste logic into actions.js

--- src/background.js
+++ src/background.js
@@ -1,15 +1,9 @@
 browser.commands.onCommand.addListener(async (command) => {
-  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-  const url = new URL(tab.url);
-
   if (command === "copy-path") {
-    const pathWithQueryAndFragment = url.pathname + url.search + url.hash;
-    await navigator.clipboard.writeText(pathWithQueryAndFragment);
+    await copyPath();
   }
 
   if (command === "paste-path") {
-    const clipboardText = await navigator.clipboard.readText();
-    const newUrl = url.origin + clipboardText;
-    await browser.tabs.update(tab.id, { url: newUrl });
+    await pastePath();
   }
 });
